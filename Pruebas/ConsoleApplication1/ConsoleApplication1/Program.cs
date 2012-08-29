037d34eee3e3c9c9d30446edcfe3e3e61460fe90
Cesar Campos <ccampos@dcc.uchile.cl>
2012-08-29 17:09:06 -0400

    Limpiado codigo, se saco Salientes del modelo

--- Pruebas/ConsoleApplication1/ConsoleApplication1/Program.cs
+++ Pruebas/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -8,18 +8,26 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            
+            
             Console.WriteLine("Inicio de generador ATNS");
             
             PatronesATNS todosATNS = new PatronesATNS();
-            
+            System.IO.File.WriteAllText(@"D:\Proyecto_Andes\Git\rostering\Pruebas\patrones.txt", todosATNS.text);
+            todosATNS.diasLibres() ;
+
+            System.IO.File.WriteAllText(@"D:\Proyecto_Andes\Git\rostering\Pruebas\patrones_dias_Libres.txt", todosATNS.text);
+//            todosATNS.diasSalientes();
+
+            todosATNS.clean();
+            todosATNS.nuevoText();
+            System.IO.File.WriteAllText(@"D:\Proyecto_Andes\Git\rostering\Pruebas\patrones_dias_libres_limpios.txt", todosATNS.text);
             //descomentar para ver salida
             //Console.Write(todosATNS.toText);
 
             //Descomentar para generar archivos
             //System.IO.File.WriteAllText(@"D:\Proyecto_Andes\Git\rostering\Pruebas\patrones.txt", todosATNS.toText);
 
-
-            
             string newline = Console.ReadLine();
         }    
     }
