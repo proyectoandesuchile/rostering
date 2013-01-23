f7f770bdafdaf18a4320a628feb0e95e0415b4fe
Cesar Campos <ccampos@dcc.uchile.cl>
2013-01-23 14:05:49 -0300

    Archivos modificados desde la ultima vez, no recuerdo que eran

---
+++ SistemaRostering/src/sistemarostering/SistemaRostering.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistemarostering;
+
+/**
+ *
+ * @author Cesar
+ */
+public class SistemaRostering {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        VentanaInicial inicio = new VentanaInicial();
+        inicio.show();
+    }
+}
